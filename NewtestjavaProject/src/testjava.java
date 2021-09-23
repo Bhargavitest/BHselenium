7fe0876e16704e8291fd2d1c0b6ff0fc6a516261
00004375 <00004375@AGI-LT-1387.arisglobal.com>
2021-09-23 18:15:08 +0530

    First commit

---
+++ NewtestjavaProject/src/testjava.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	
+public class testjava {
+		 public static void main(String[] args) throws InterruptedException
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\selenium\\chromedriver.exe");
+			 WebDriver driver=new ChromeDriver();
+
+	         driver.get("https://www.linkedin.com/login");
+
+	        driver.findElement(By.id("username")).sendKeys("admin");
+
+	        driver.findElement(By.name("session_password")).sendKeys("123");
+
+	        driver.findElement(By.xpath(" //*[@id=\"organic-div\"]/form/div[3]/button")).click();
+
+	        Thread.sleep(5000);
+	       WebElement printTxt=driver.findElement(By.id("error-for-username"));
+	          String txt=printTxt.getText();
+	          System.out.println(txt);
+
+	          driver.findElement(By.linkText("Forgot password?")).click();
+	           Thread.sleep(5000);
+
+	           driver.findElement(By.partialLinkText("Sign")).click();
+
+	        driver.close();
+
+	}
+	}
